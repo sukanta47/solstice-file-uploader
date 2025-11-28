8047f7077e9ce7907ab2c443e49399dd01505f11
sukanta47 <sukantabiswas.dev@gmail.com>
2025-11-28 13:03:27 +0530

    updated imports

--- src/modules/auth/__test__/AuthTest.test.tsx
+++ src/modules/auth/__test__/AuthTest.test.tsx
@@ -1,17 +1,21 @@
 import { describe, expect, it } from "vitest";
 import LoginPage from "../pages/LoginPage";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../../utils/test-utils";
+import LoginForm from "../components/LoginForm";
 
 describe("Auth module test", () => {
   it("tests login page redering", () => {
     renderWithProviders(<LoginPage />);
     expect(screen.getByText("File Uploader")).toBeInTheDocument();
     expect(screen.getByText("File Uploader")).toBeInTheDocument();
-    expect(screen.getByRole("h1")).toHaveDisplayValue("Login");
+    expect(screen.getByText("Login")).toBeInTheDocument();
     expect(
       screen.getByText("By continuing, you agree to our")
     ).toBeInTheDocument();
     expect(screen.getByText("Terms of Service")).toBeInTheDocument();
+  });
+  it("tests the Login form", () => {
+    renderWithProviders(<LoginForm />);
   });
 });
